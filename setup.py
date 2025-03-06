42174d1a2aa010d0bb2c0d0759ca766e70ed8c6b
ARHCOS <72146549+ARHCOS@users.noreply.github.com>
2025-03-07 00:18:47 +0100

    Update setup.py
    Added the creation of the CSV folder in setup

--- setup.py
+++ setup.py
@@ -4,12 +4,21 @@ import os
 
 def install_modules(modules):
     for module in modules:
-        print(f"Installing : {module}...")
+        print(f"Installing : {module}...\n")
         subprocess.check_call([sys.executable, "-m", "pip", "install", module])
 
 modules_to_install = ['pandas', 'numpy']
 
 current_folder = os.path.dirname(os.path.abspath(__file__))
 script_folder = os.path.join(current_folder, 'scripts')
+csv_folder_path = current_folder + "\\CSV"
+
+if not os.path.exists(csv_folder_path):
+    os.makedirs(csv_folder_path)
+    print("Created the CSV folder\n")
+else:
+    print("The CSV folder already exists\n")
+
+print("Installing modules...\n")
 
 install_modules(modules_to_install)
